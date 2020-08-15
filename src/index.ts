913b8ddba04496325f87bcbf2b8272182a2905e7
Vince Smith <VinceS22@gmail.com>
2020-08-15 02:59:52 -0400

    Linting file and some implementation

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,39 @@
-import * as Discord from 'discord.js'
+import * as Discord from "discord.js";
 import {IncomingMessage} from "http";
-const https = require('https');
+const https = require("https");
 const client = new Discord.Client();
 
-const settings = require('./settings.json');
+const settings = require("./settings.json");
+const domParser = new DOMParser();
+client.once("ready", () => {
+    client.on("message", (message) => {
+        console.log(message.content);
 
-client.once('ready', () => {
-    client.on('message', (message) => {
-        console.log(message.content)
-
-        if(message.content === "!help"){
-            message.channel.send('This is the help. Im helping! :)')
-            getWebPage(settings.baseUrl);
+        if (message.content === "!help") {
+            message.channel.send("This is the help. Im helping! :)");
+            getWebPage(settings.baseUrl + ",goto,112").then((data) => {
+                console.log(data);
+                const doc = domParser.parseFromString(data, "text/html");
+            });
         }
     });
 });
 
 client.login(settings.token);
 
-
 //  Get method implementation:
-function getWebPage(url = '', data = {}) {
-    https.get(url, (response: IncomingMessage) => {
-        let data = '';
-        response.on('data', (chunk) => {
-            data += chunk;
+async function getWebPage(url = "", data = {}): Promise<string> {
+    return https.get(url, (response: IncomingMessage) => {
+        let responseData = "";
+        response.on("data", (chunk) => {
+            responseData += chunk;
         });
 
-        response.on('end', () => {
-            console.log(data)
+        response.on("end", () => {
+            return responseData;
         });
-    }).on('error', (err:Error) => {console.log("Error: " + err.message)});
+    }).on("error", (err: Error) => {
+        console.log("Error: " + err.message);
+        return err.message;
+    });
 }
