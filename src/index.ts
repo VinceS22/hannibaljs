055dcfc3cf4a7c48cf26dc4fcbb838dc39154ace
Vince Smith <VinceS22@gmail.com>
2020-08-16 18:31:21 -0400

    Acceptance/Rejection Implementation
    Hannibal 9000 will now (hopefully) be able to tell if a post is an acceptance/rejection.
    
    It will output: "[Poster] has accepted [applicantName]"

--- src/index.ts
+++ src/index.ts
@@ -15,18 +15,40 @@ client.once("ready", () => {
             message.channel.send("This is the help. Im helping! :)");
             getWebPage(settings.baseUrl + ",goto," + currentPage).then((data) => {
                 const $ = cheerio.load(data);
+
                 $("article.forum-post").map((index: number, element: CheerioElement) => {
                     const userName = $("h3", element).data("displayname").replace("%A0", " ");
                     const postContent = $(".forum-post__body", element).eq(0).contents();
                     let resultString = "";
+                    let appUsername = "";
+                    let purpose = postPurpose.Bump;
                     postContent.each((i, elem) => {
-                        resultString += renderElement(elem);
+                        const renderedElement = renderElement(elem);
+                        resultString += renderedElement.postText;
+                        if (renderedElement.purpose !== postPurpose.Bump) {
+                            purpose = postPurpose.Acceptance;
+                            purpose = renderedElement.purpose;
+                        }
+                        if(renderedElement.appUsername) {
+                            appUsername = renderedElement.appUsername;
+                        }
                     });
 
                     console.log("Username: " + userName);
+                    console.log("Post Purpose: " + purpose.toString());
                     console.log("Post content: " + resultString);
-                    results += "Current Username: " + userName + " \n " + "-------------------------------" + "\n" +
-                        resultString + "\n\n";
+                    results += "Post purpose: " + purpose + "\n Current Poster's Username: " +
+                      userName + " \n " + "-------------------------------" + "\n";
+                    // @ts-ignore
+                    if (purpose === postPurpose.Acceptance)
+                    {
+                        results += userName + " has accepted " + appUsername + "\n";
+                    } else {
+                        // @ts-ignore
+                        if (purpose === postPurpose.Rejection) {
+                                                results += userName + " has rejected " + appUsername + "\n";
+                                            }
+                    }
                     if (results.length > 1000) {
                         message.channel.send(results);
                         results = "";
@@ -45,30 +67,63 @@ client.once("ready", () => {
 
 client.login(settings.token);
 
-const renderElement = (elem: CheerioElement): string => {
-    let resultString = "";
+enum postPurpose {
+    Bump="Bump",
+    Application="Application",
+    Acceptance="Acceptance",
+    Rejection="Rejection",
+}
+interface IPostResults {
+    postText: string;
+    purpose: postPurpose;
+    appUsername: string;
+}
+// Takes a line in a post, determines what it is, then sends a string back depending on what it is.
+const renderElement = (elem: CheerioElement): IPostResults => {
+    let postText = "";
+    let purpose: postPurpose = postPurpose.Bump;
+    let appUsername = "";
     if (elem.type === "text" && elem.data) { // Line with actual text in it
-        resultString += elem.data;
+        postText += elem.data;
+
+        if (elem.data.includes("Username: ")) {
+            appUsername = elem.data.split(" ")[1];
+        } else if(elem.data.includes(settings.acceptanceString)) {
+            purpose = postPurpose.Acceptance;
+        } else if(elem.data.includes(settings.rejectionString)) {
+            purpose = postPurpose.Rejection;
+        }
+
     } else if (elem.type === "tag" && elem.name === "br") { // Standard linebreak
-        resultString += "\n";
+        postText += "\n";
     } else if (elem.type === "tag" && elem.name === "span") { // This is a quoted post
         let spanContents = "";
         spanContents = elem.children.map((nestedElement) => {
-            return renderElement(nestedElement);
+            const elementContent = renderElement(nestedElement);
+            // Yank the username from the quoted text and set it if we have it.
+            if (elementContent.appUsername) {
+                appUsername = elementContent.appUsername;
+            }
+            return elementContent.postText;
         }).join("");
-        resultString += "-----START QUOTE-----\n" + spanContents + "\n-----END QUOTE-----";
+        postText += "-----START QUOTE-----\n" + spanContents + "\n-----END QUOTE-----";
     } else if (elem.type === "tag" && elem.name === "div") { // A div with more elements in it
-        resultString += elem.children.map((nestedElement) => {
-            return renderElement(nestedElement);
-        }).join("");
+        elem.children.forEach((nestedElement) => {
+            const element = renderElement(nestedElement);
+            postText += element.postText;
+            if(element.purpose === postPurpose.Acceptance || element.purpose === postPurpose.Rejection) {
+                purpose = element.purpose;
+            }
+        });
+
+
     } else {
         console.log(elem.data);
     }
-    return resultString;
-
+    return {appUsername, purpose, postText };
 };
 
-//  Get method implementation:
+//  HTTP Get method implementation:
 async function getWebPage(url = "", data = {}): Promise<string>  {
     return new Promise((resolve, reject) => {
         https.get(url, (response: IncomingMessage) => {
