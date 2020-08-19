75392325c2c1d17943b36c4d1825b94d3c7604e3
Vince Smith <VinceS22@gmail.com>
2020-08-19 17:44:21 -0400

    Remember prior call + polling
    Remember prior call. Polling is not fully working yet.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import {IncomingMessage} from "http";
 import cheerio from "cheerio";
 import https from "https";
 import settings from "./settings.json";
+import {Message} from "discord.js";
 
 const client = new Discord.Client();
 
@@ -13,89 +14,91 @@ client.once("ready", () => {
     let lastPage = -1;
     const debug = false;
     let bumpers: {[poster: string]: number} = {};
+    let priorBumpers: {[poster: string]: number} = {};
     let applicants: {[poster: string]: boolean} = {};
-    const promises: any[] = [];
-    client.on("message", async (message) => {
-        if (message.content === "!forum") {
-            message.channel.send("This is the help. Im helping! :)");
-            // True if the user has a corresponding accept/reject
-            promises.push(getWebPage(settings.baseUrl).then((pageNumData) => {
-                let $ = cheerio.load(pageNumData);
-                lastPage = parseInt($("input[title='Page Number']").prop("max") ?? -1);
-                currentPage = lastPage - 1;
-                results += "Sharing page " + currentPage + " and " + lastPage + "\n";
-                for (currentPage; currentPage <= lastPage; currentPage++) {
-                    getWebPage(settings.baseUrl + ",goto," + currentPage).then((data) => {
-                        console.log('Web call');
-                        $ = cheerio.load(data);
-                        lastPage = parseInt($("input[title='Page Number']").prop("max") ?? -1);
-                        $("article.forum-post").map((index: number, element: CheerioElement) => {
-                            const userName = $("h3", element).data("displayname").replace("%A0", " ");
-                            const postContent = $(".forum-post__body", element).eq(0).contents();
-                            let resultString = "";
-                            let appUsername = "";
-                            let purpose = postPurpose.Bump;
-                            postContent.each((i, elem) => {
-                                const renderedElement = renderElement(elem);
-                                resultString += renderedElement.postText;
-                                if (renderedElement.purpose !== postPurpose.Bump) {
-                                    purpose = renderedElement.purpose;
-                                }
-                                if(renderedElement.appUsername) {
-                                    appUsername = renderedElement.appUsername;
-                                }
-                            });
+    let priorApplicants: {[poster: string]: boolean} = {};
+    let shouldPollForums: boolean = false;
+    let hasNewPost: boolean = false;
 
-                            // @ts-ignore
-                            if (purpose === postPurpose.Acceptance) {
-                                results += userName + " has accepted " + appUsername + "\n";
-                                applicants[appUsername] = true;
-                            } else { // @ts-ignore
-                                if (purpose === postPurpose.Rejection) {
-                                    results += userName + " has rejected " + appUsername + "\n";
-                                    applicants[appUsername] = true;
-                                } else if (appUsername.length > 0) {
-                                    purpose = postPurpose.Application;
-                                }
+    const promises: any[] = [];
+
+    function checkForums(message: Message, notifyMeOnNoNewPosts: boolean = true) {
+        // True if the user has a corresponding accept/reject
+        promises.push(getWebPage(settings.baseUrl).then((pageNumData) => {
+            let $ = cheerio.load(pageNumData);
+            lastPage = parseInt($("input[title='Page Number']").prop("max") ?? -1);
+            currentPage = lastPage - 1;
+            results += "Checking page " + currentPage + " and " + lastPage + "...\n";
+            for (currentPage; currentPage <= lastPage; currentPage++) {
+                getWebPage(settings.baseUrl + ",goto," + currentPage).then((data) => {
+                    console.log('Web call');
+                    $ = cheerio.load(data);
+                    lastPage = parseInt($("input[title='Page Number']").prop("max") ?? -1);
+                    $("article.forum-post").map((index: number, element: CheerioElement) => {
+                        const userName = $("h3", element).data("displayname").replace("%A0", " ");
+                        const postContent = $(".forum-post__body", element).eq(0).contents();
+                        let resultString = "";
+                        let appUsername = "";
+                        let purpose = postPurpose.Bump;
+                        postContent.each((i, elem) => {
+                            const renderedElement = renderElement(elem);
+                            resultString += renderedElement.postText;
+                            if (renderedElement.purpose !== postPurpose.Bump) {
+                                purpose = renderedElement.purpose;
                             }
-                            if (debug) {
-                                results += "Current Poster's Username: " + userName + "\n" + "Post purpose: " +
-                                  purpose + "\n" ;
-                            }
-                            switch (purpose) {
-                                case postPurpose.Bump:
-                                    if (bumpers[userName]) {
-                                        bumpers[userName]++;
-                                    } else {
-                                        bumpers[userName] = 1;
-                                    }
-                                    break;
-                              // @ts-ignore
-                                case postPurpose.Acceptance: // @ts-ignore
-                                    applicants[appUsername] = true;
-                                    break;
-                              // @ts-ignore
-                                case postPurpose.Rejection: // @ts-ignore
-                                    applicants[appUsername] = true;
-                                    break;
-                                case postPurpose.Application:
-                                    if (!applicants[appUsername]) {
-                                        applicants[appUsername] = false;
-                                    }
-                                    break;
+                            if (renderedElement.appUsername) {
+                                appUsername = renderedElement.appUsername;
                             }
                         });
 
+                        // @ts-ignore
+                        if (purpose === postPurpose.Acceptance || purpose === postPurpose.Rejection) {
+                            applicants[appUsername] = true;
+                        } else if (appUsername.length > 0) {
+                            purpose = postPurpose.Application;
+                        }
+                        if (debug) {
+                            results += "Current Poster's Username: " + userName + "\n" + "Post purpose: " +
+                              purpose + "\n";
+                        }
+                        switch (purpose) {
+                            case postPurpose.Bump:
+                                if (bumpers[userName]) {
+                                    bumpers[userName]++;
+                                } else {
+                                    bumpers[userName] = 1;
+                                }
+                                break;
+                          // @ts-ignore
+                            case postPurpose.Acceptance: // @ts-ignore
+                                applicants[appUsername] = true;
+                                break;
+                          // @ts-ignore
+                            case postPurpose.Rejection: // @ts-ignore
+                                applicants[appUsername] = true;
+                                break;
+                            case postPurpose.Application:
+                                if (!applicants[appUsername]) {
+                                    applicants[appUsername] = false;
+                                }
+                                break;
+                        }
                     });
-                }
-            }));
-            // This is using a timeout. It's gross and I hate it.
-            // TODO: Figure out how to incorporate promises correctly for this.
-            setTimeout(() => {
-                for (const [key, value] of Object.entries(bumpers)) {
+
+                });
+            }
+        }));
+        // This is using a timeout. It's gross and I hate it.
+        // TODO: Figure out how to incorporate promises correctly for this.
+        setTimeout(() => {
+            for (const [key, value] of Object.entries(bumpers)) {
+                if (bumpers[key] !== priorBumpers[key]) {
                     results += key + " has bumped the thread " + value + " times\n";
+                    hasNewPost = true;
                 }
-                for (const [key, value] of Object.entries(applicants)) {
+            }
+            for (const [key, value] of Object.entries(applicants)) {
+                if (priorApplicants[key] !== applicants[key]) {
                     results += key + " has applied";
                     if (value) {
                         results += " and has been processed \n";
@@ -104,15 +107,39 @@ client.once("ready", () => {
                           ",goto," + currentPage + "\n";
                         results += " Here\'s your command: !rw " + key + "\n";
                     }
+                    hasNewPost = true;
                 }
+            }
+            if(hasNewPost || debug) {
+                message.channel.send(results);
+            } else {
+                message.channel.send("Nothing new!");
+            }
+            priorBumpers = bumpers;
+            priorApplicants = applicants;
+            bumpers = {};
+            applicants = {};
+            results = "";
+            hasNewPost = false;
+        }, 10 * 1000); // * 1000 makes it seconds
+    }
 
-                if (results.length > 0) {
-                    message.channel.send(results);
-                }
-                bumpers = {};
-                applicants = {};
-                results = "";
-            }, 10000);
+    client.on("message", async (message) => {
+        if (message.content === "!forums") {
+            message.channel.send("Checking forums now.");
+            checkForums(message);
+        } else if (message.content === "!pollforums") {
+            message.channel.send("Activating poll mode.");
+            shouldPollForums = true;
+            while (shouldPollForums) {
+                checkForums(message);
+                setInterval(() => {
+                    checkForums(message);
+                }, 1000 * 20);
+            }
+        } else if (message.content === "!stoppollforums") {
+            message.channel.send("Deactivating poll mode.");
+            shouldPollForums = false;
         }
     });
 });
